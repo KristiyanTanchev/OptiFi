526c6f2d84aeb7e9d11d1cdd687395c3dcdba91f
KristiyanTanchev <kristiyan.tanchev@gmail.com>
2026-03-10 23:28:17 +0200

    test(integration): refactor testcontainers setup to static singleton

--- backend/src/test/java/com/optifi/integration/support/AbstractIntegrationTest.java
+++ backend/src/test/java/com/optifi/integration/support/AbstractIntegrationTest.java
@@ -6,22 +6,29 @@ import com.optifi.domain.category.repository.CategoryRepository;
 import com.optifi.domain.transaction.repository.TransactionRepository;
 import com.optifi.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-@Testcontainers
 public abstract class AbstractIntegrationTest {
+
+    private static final PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:15-alpine")
+                    .withDatabaseName("optifi_test")
+                    .withUsername("optifi")
+                    .withPassword("optifi");
+
+    static {
+        postgres.start();
+    }
 
     @Autowired
     private AccountRepository accountRepository;
@@ -41,12 +48,6 @@ public abstract class AbstractIntegrationTest {
     @Autowired
     protected MockMvc mockMvc;
 
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
-            .withDatabaseName("optifi_test")
-            .withUsername("optifi")
-            .withPassword("optifi");
-
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
@@ -57,10 +58,10 @@ public abstract class AbstractIntegrationTest {
 
     @AfterEach
     void cleanUp() {
-        accountRepository.deleteAll();
-        budgetRepository.deleteAll();
-        categoryRepository.deleteAll();
         transactionRepository.deleteAll();
+        budgetRepository.deleteAll();
+        accountRepository.deleteAll();
+        categoryRepository.deleteAll();
         userRepository.deleteAll();
     }
 }
